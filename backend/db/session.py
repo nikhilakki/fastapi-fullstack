b384f6786e58a058a6eafb2c3792cd13dde57e94
Nikhil Akki <17634231+nikhilakki@users.noreply.github.com>
2021-09-08 10:42:18 +0530

    minor logic refactor

--- backend/db/session.py
+++ backend/db/session.py
@@ -3,7 +3,6 @@
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
 
-import os
 from typing import Generator
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -12,17 +11,15 @@ from core.config import settings
 
 MODE = settings.MODE
 
+
 if MODE == "prod":
-    print("using a SQL DB")
+    print("using a SQL DB") 
     SQLALCHEMY_DATABASE_URL = settings.DATABASE_URL
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
 else:
     print("Using SQLite3")
     SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-    engine = create_engine(
-        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-    )
 
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
